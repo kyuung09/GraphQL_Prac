7faaa16fddc86e5391a1ee7648dc07ebefca7bec
kyuu <kyuung09@gmail.com>
2023-04-06 14:56:32 +0900

    feat : MyBatis + Graphql 기능 테스트 코드 작성

--- src/test/java/com/example/graphql_prac_/GraphQlPracApplicationTests.java
+++ src/test/java/com/example/graphql_prac_/GraphQlPracApplicationTests.java
@@ -1,21 +1,60 @@
 package com.example.graphql_prac_;
 
+import com.example.graphql_prac_.DTO.UserRequestDto;
 import com.example.graphql_prac_.Repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 
-@DataJpaTest
-//@SpringBootTest
+//@DataJpaTest
+@SpringBootTest
 class GraphQlPracApplicationTests {
 
     @Autowired
     private UserRepository userRepository;
-//
-//    @Test
-//    @Transactional
-//    @Rollback
-//    @DisplayName("mybatis 회원가입 테스트")
-//    public void memberSaveTest1() {
-//        userRepository.save1(new UserMapperDTO("mybatis회원이메일", "mybatis회원비밀번호", "mybatis회원이름"));
-//    }
+
+    @Test
+    @Order(1)
+    @Transactional
+    @Rollback
+    @DisplayName("사용자 생성")
+    public void createUserTest() {
+        userRepository.createUser(new UserRequestDto(1, "kyuu@1234.com", "1234", "kyuu"));
+    }
+
+    @Test
+    @Order(2)
+    @Transactional
+    @Rollback
+    @DisplayName("사용자 정보 업데이트")
+    public void updateUserTest() {
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                        .user_id(1)
+                        .build();
+        userRepository.updateUser(userRequestDto);
+    }
+
+    @Test
+    @Order(3)
+    @Transactional
+    @Rollback
+    @DisplayName("사용자 삭제")
+    public void deleteUserTest() {
+        UserRequestDto userRequestDto = UserRequestDto.builder()
+                .user_id(1)
+                .build();
+        userRepository.deleteUser(userRequestDto);
+    }
+
+
+    @AfterEach
+    @Transactional
+    @Rollback
+    @DisplayName("사용자 조회")
+    public void selectUserTest() {
+        userRepository.getUsers();
+    }
+
 }
